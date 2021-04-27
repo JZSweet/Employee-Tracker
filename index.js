d947274a3f8cba1763b2b09d7a1d1be339a26bf3
JZSweet <sweet_zhaozhao@live.cn>
2021-04-27 12:08:14 -0700

    add view function

--- index.js
+++ index.js
@@ -1,37 +1,100 @@
-const inquirer = require("inquirer");
-var mysql = require("mysql");
-console.log("hello");
+const inquirer = require('inquirer');
+var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: 'localhost',
   port: 3306,
-  user: "root",
-  password: "SweetZhao1968!",
-  database: "employee_db"
+  user: 'root',
+  password: 'SweetZhao1968!',
+  responsebase: 'employee_db'
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  
-  // inquirer
+  console.log('connected as id ' + connection.threadId + '\n');
+  startMenuMenu();
+});
+
+// inquirer
+function startMenuMenu() {
   inquirer
     .prompt([
-        {
-            type: 'input',
-            message: 'whats your role',
-            name: 'role',
-  
-        },
+      {
+        name: 'command',
+        type: 'list',
+        message: 'Welcome to the employee tracker. Please insert your command.',
+        choices: ['View', 'Add', 'Update', 'Exit']
+      },
+      {
+        name: 'type',
+        type: 'list',
+        message: 'Select type',
+        choices: ['Employees', 'Roles', 'Departments']
+      },
     ])
     .then((response) => {
-       console.log("finished",response)
-       connection.query("SELECT * FROM employee_db.department;",function(err,res){
-         console.log(res)
-       })
-       connection.end()
-    });
-});
 
-// MYsql databass
-// seed sqls
+      switch (response.command) {
+        case 'View all employees':
+          viewEmployee();
+          break;
+
+        case 'View all roles':
+          viewRole();
+          break;
+
+        case 'View all departments':
+          viewDepartment();
+          break;
+      }
+      break;
+      
+    // Add
+      // switch (response.type) {
+      //   case 'Employees':
+      //     newEmployee();
+      //     break;
+
+      //   case 'Role':
+      //     newRole();
+      //     break;
+
+      //   case 'Department':
+      //     newDepartment();
+      //     break;
+
+      //   case 'Edit':
+      //     Edit();
+      //     break;
+      // }
+      // break;
+
+    });
+  connection.end()
+};
+
+function viewEmployee() {
+  connection.query('SELECT * FROM employee', (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    startMenu();
+  });
+};
+
+function viewRole() {
+  connection.query('SELECT * FROM role', (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    startMenu();
+  });
+};
+
+function viewDepartment() {
+  connection.query('SELECT * FROM department', (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    startMenu();
+  });
+};
+
+
--- seed.sql
+++ seed.sql
@@ -4,7 +4,25 @@ USE employee_db;
 
 CREATE TABLE department (
 id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-name VARCHAR(30)
+name VARCHAR(30) NOT NULL,
 );
+
+CREATE TABLE role (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department (id),
+);
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role (id),
+  manager_id INT,
+);
+
 INSERT INTO department (name)
 VALUES ('sales');
